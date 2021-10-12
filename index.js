e9afcbd72e44ba3531a1bad23865d6dcb9819379
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2021-10-12 20:19:32 +0100

    #1: update position and color of the plane. add a util function for converting rows data into vertices.

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
+import { convertRowsToVertices } from "./utils.js";
+
+const vertices = convertRowsToVertices([
+	[
+		[0.0, 3.0, 0.0],
+		[0.5, 3.0, 0.0],
+		[1.0, 3.0, 0.0]
+	],
+	[
+		[0.0, 3.0, 0.5],
+		[0.5, 3.0, 0.5],
+		[1.0, 3.0, 0.5]
+	]
+])
+
+const colors = convertRowsToVertices([
+	[
+		[0.0, 0.0, 0.0],
+		[0.0, 0.0, 0.5],
+		[0.0, 0.0, 1.0]
+	],
+	[
+		[0.0, 1.0, 0.0],
+		[0.5, 0.5, 0.0],
+		[1.0, 0.0, 0.0]
+	]
+])
+
 const scene = document.getElementById('scene').object3D
 
-const getPlane = () => {
+let plane;
+
+const createPlane = (nVertices) => {
 	const vertexShader = `
 		attribute vec3 color;
 	
@@ -21,47 +51,33 @@ const getPlane = () => {
 	`
 
 	const planeGeometry = new THREE.BufferGeometry()
-	const planeVertices = new Float32Array([
-		0.0, 2.0, 0.0,
-		0.0, 2.0, 1.0,
-		1.0, 2.0, 1.0,
 
-		1.0, 2.0, 1.0,
-		1.0, 2.0, 0.0,
-		0.0, 2.0, 0.0
-	])
-	const colors = new Float32Array([
-		1.0, 0.0, 0.0,
-		0.0, 1.0, 0.0,
-		0.0, 0.0, 1.0,
-
-		0.0, 0.0, 1.0,
-		1.0, 0.0, 1.0,
-		1.0, 0.0, 0.0,
-	])
+	const planeVertices = new Float32Array(nVertices * 3)
+	const colors = new Float32Array(nVertices * 3)
 	planeGeometry.setAttribute('position', new THREE.BufferAttribute(planeVertices, 3))
 	planeGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
 
 	const planeMaterial = new THREE.ShaderMaterial({
 		vertexShader,
 		fragmentShader,
+		side: THREE.DoubleSide
 	})
-
 	const plane = new THREE.Mesh(planeGeometry, planeMaterial)
+
 	return plane
 }
 
-const sceneInit = () => {
-	const geometry = new THREE.BoxGeometry()
-	const material = new THREE.MeshStandardMaterial({ color: 0xff0000, opacity: 0.8 })
-	const cube = new THREE.Mesh(geometry, material)
-	cube.position.z = 2
-	cube.position.y = 2
-	scene.add(cube)
-
-	scene.add(getPlane())
-
-	console.log(scene)
+const updatePlaneVertices = (newVertices) => {
+	plane.geometry.attributes.position.array = newVertices
+	plane.geometry.attributes.position.needsUpdate = true;
 }
 
-sceneInit();
+const updatePlaneColors = (newColors) => {
+	plane.geometry.attributes.color.array = newColors
+	plane.geometry.attributes.color.needsUpdate = true;
+}
+
+plane = createPlane(vertices.length / 3)
+scene.add(plane)
+updatePlaneVertices(vertices)
+updatePlaneColors(colors)
---
+++ utils.js
@@ -0,0 +1,24 @@
+/**
+ * Given some data in rows, return the triangulated vertices.
+ * @param {number[][][]} rows Each point is a 3D array. Each row is an array of points.
+ * @returns 
+ */
+const convertRowsToVertices = (rows) => {
+	const verticesArray = [];
+	for (let i = 0; i < rows.length - 1; i++) {
+		const row = rows[i]
+		const nextRow = rows[i + 1]
+
+		const verticesRow = []
+		for (let j = 0; j < row.length - 1; j++) {
+			verticesRow.push(...row[j], ...row[j + 1], ...nextRow[j])
+			verticesRow.push(...row[j + 1], ...nextRow[j], ...nextRow[j + 1])
+		}
+
+		verticesArray.push(...verticesRow)
+	}
+
+	return new Float32Array(verticesArray)
+}
+
+export { convertRowsToVertices }
